f9c190ead15c7b873e6f0771bc864eedda8c2b6e
FANNY PRETRE <fanny.pretre@gmail.com>
2024-06-11 16:27:08 +0200

    Schema SQL and table creation : done

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,4 +1,9 @@
 -- SQLBook: Code
+create table role (
+  id INT unsigned PRIMARY KEY auto_increment NOT NULL,
+  name VARCHAR(255) NOT NULL
+);
+
 create table user (
   id INT unsigned PRIMARY KEY auto_increment NOT NULL,
   email VARCHAR(255) NOT NULL unique,
@@ -9,45 +14,32 @@ create table user (
   address VARCHAR(255) NOT NULL,
   zip_code INT NOT NULL,
   city VARCHAR(255) NOT NULL, 
-  role_id INT NOT NULL,
-      CONSTRAINT fk_user_role      
-      FOREIGN KEY (role_id)             
-      REFERENCES role(id)   
+  role_id INT UNSIGNED NOT NULL,
+  FOREIGN KEY(role_id) REFERENCES role(id)
 );
 
-create table role (
+create table brand (
   id INT unsigned PRIMARY KEY auto_increment NOT NULL,
-  name VARCHAR(255) NOT NULL,
-);
-
-create table car (
-  id INT unsigned PRIMARY KEY auto_increment NOT NULL,
-  image TEXT NOT NULL,
-  user_id INT NOT NULL,
-      CONSTRAINT fk_car_user     
-      FOREIGN KEY (user_id)             
-      REFERENCES user(id),
-  model_id INT NOT NULL,
-      CONSTRAINT fk_car_model     
-      FOREIGN KEY (model_id)             
-      REFERENCES model(id)
-  
+  name VARCHAR(255) NOT NULL
 );
 
 create table model (
   id INT unsigned PRIMARY KEY auto_increment NOT NULL,
   name VARCHAR(255) NOT NULL,
   plug_type VARCHAR(255) NOT NULL,
-  brand_id INT NOT NULL,
-      CONSTRAINT fk_model_brand    
-      FOREIGN KEY (brand_id)             
-      REFERENCES brand(id)
+  brand_id INT UNSIGNED NOT NULL,
+  FOREIGN KEY(brand_id) REFERENCES brand(id)
 );
 
-create table brand (
+create table car (
   id INT unsigned PRIMARY KEY auto_increment NOT NULL,
-  name VARCHAR(255) NOT NULL,
+  image TEXT NOT NULL,
+  user_id INT UNSIGNED NOT NULL,
+  FOREIGN KEY(user_id) REFERENCES user(id),
+  model_id INT UNSIGNED NOT NULL,
+  FOREIGN KEY(model_id) REFERENCES model(id)
 );
+
 
 create table terminal (
   id INT unsigned PRIMARY KEY auto_increment NOT NULL,
@@ -59,7 +51,7 @@ create table terminal (
   power VARCHAR(80),
   plug_type VARCHAR(255) NOT NULL,
   chain_name VARCHAR(255),
-  accessibility VARCHAR(255),
+  accessibility VARCHAR(255)
 );
 
 create table booking (
@@ -67,12 +59,8 @@ id INT unsigned PRIMARY KEY auto_increment NOT NULL,
 date DATE NOT NULL, 
 starting_hour TIME NOT NULL, 
 end_hour TIME NOT NULL, 
-terminal_id INT NOT NULL,
-      CONSTRAINT fk_booking_terminal     
-      FOREIGN KEY (terminal_id)             
-      REFERENCES terminal(id),
-user_id INT NOT NULL,
-      CONSTRAINT fk_booking_user     
-      FOREIGN KEY (user_id)             
-      REFERENCES user(id),
+terminal_id INT UNSIGNED NOT NULL,
+FOREIGN KEY(terminal_id) REFERENCES terminal(id),
+user_id INT UNSIGNED NOT NULL,
+FOREIGN KEY(user_id) REFERENCES user(id)
 );
