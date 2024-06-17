853347aef7ec392aeddf2bbcc1be149c11f299b3
Vincent Pasquiou <vincent.pasquiou@gmail.com>
2024-06-17 12:25:01 +0200

    created link to edit a user and a car

--- client/src/pages/Admin/AdminUtilisateurs.jsx
+++ client/src/pages/Admin/AdminUtilisateurs.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, Link } from "react-router-dom";
 import { useState } from "react";
 
 import "./styles/AdminUtilisateurs.scss";
@@ -49,14 +49,16 @@ function AdminUtilisateur() {
               <p>{user.firstname}</p>
               <p>{user.lastname}</p>
             </div>
-            <svg
-              viewBox="0 0 24 24"
-              height={20}
-              width={20}
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="m4.481 15.659c-1.334 3.916-1.48 4.232-1.48 4.587 0 .528.46.749.749.749.352 0 .668-.137 4.574-1.492zm1.06-1.061 3.846 3.846 11.321-11.311c.195-.195.293-.45.293-.707 0-.255-.098-.51-.293-.706-.692-.691-1.742-1.74-2.435-2.432-.195-.195-.451-.293-.707-.293-.254 0-.51.098-.706.293z" />
-            </svg>
+            <Link to={`/administrateur/utilisateurs/${user.id}`}>
+              <svg
+                viewBox="0 0 24 24"
+                height={20}
+                width={20}
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="m4.481 15.659c-1.334 3.916-1.48 4.232-1.48 4.587 0 .528.46.749.749.749.352 0 .668-.137 4.574-1.492zm1.06-1.061 3.846 3.846 11.321-11.311c.195-.195.293-.45.293-.707 0-.255-.098-.51-.293-.706-.692-.691-1.742-1.74-2.435-2.432-.195-.195-.451-.293-.707-.293-.254 0-.51.098-.706.293z" />
+              </svg>
+            </Link>
             <svg
               viewBox="0 0 24 24"
               height={20}
--- client/src/pages/Admin/AdminVehicules.jsx
+++ client/src/pages/Admin/AdminVehicules.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, Link } from "react-router-dom";
 import { useState } from "react";
 
 function AdminVehicule() {
@@ -45,14 +45,16 @@ function AdminVehicule() {
               <p>{car.name}</p>
               <p>{car.id}</p>
             </div>
-            <svg
-              viewBox="0 0 24 24"
-              height={20}
-              width={20}
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="m4.481 15.659c-1.334 3.916-1.48 4.232-1.48 4.587 0 .528.46.749.749.749.352 0 .668-.137 4.574-1.492zm1.06-1.061 3.846 3.846 11.321-11.311c.195-.195.293-.45.293-.707 0-.255-.098-.51-.293-.706-.692-.691-1.742-1.74-2.435-2.432-.195-.195-.451-.293-.707-.293-.254 0-.51.098-.706.293z" />
-            </svg>
+            <Link to={`/administrateur/vehicules/${car.id}`}>
+              <svg
+                viewBox="0 0 24 24"
+                height={20}
+                width={20}
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="m4.481 15.659c-1.334 3.916-1.48 4.232-1.48 4.587 0 .528.46.749.749.749.352 0 .668-.137 4.574-1.492zm1.06-1.061 3.846 3.846 11.321-11.311c.195-.195.293-.45.293-.707 0-.255-.098-.51-.293-.706-.692-.691-1.742-1.74-2.435-2.432-.195-.195-.451-.293-.707-.293-.254 0-.51.098-.706.293z" />
+              </svg>
+            </Link>
             <svg
               viewBox="0 0 24 24"
               height={20}
