84cefc5842c8ca494e84d189b634d6e94b442ed8
Vincent Pasquiou <vincent.pasquiou@gmail.com>
2024-06-18 12:46:16 +0200

    add map display

--- client/src/pages/Carte.jsx
+++ client/src/pages/Carte.jsx
@@ -1,10 +1,25 @@
-import NavVisiteur from "../components/NavVisiteur";
+// import NavVisiteur from "../components/NavVisiteur";
+import { MapContainer, TileLayer } from "react-leaflet";
+import "./styles/Carte.scss";
 
 function Carte() {
   return (
     <section>
       <h1> Bonjour je suis Carte </h1>
-      <NavVisiteur />
+      {/* <NavVisiteur /> */}
+
+      <div id="map">
+        <MapContainer
+          center={[51.505, -0.09]}
+          zoom={13}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </MapContainer>
+      </div>
     </section>
   );
 }
