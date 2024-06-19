1609da3222619bee6caccb10e50fc6f3d540acdf
FANNY PRETRE <fanny.pretre@gmail.com>
2024-06-19 11:52:17 +0200

    Creation and scss of Home page

--- client/src/pages/Accueil.jsx
+++ client/src/pages/Accueil.jsx
@@ -1,12 +1,120 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
+import Accueil1 from "../assets/images/1.png";
+import Accueil2 from "../assets/images/2.png";
+import Accueil3 from "../assets/images/11.png";
+import Accueil4 from "../assets/images/13.png";
+
+import "./styles/Accueil.scss";
+
+const items = [
+  {
+    id: 1,
+    src: Accueil1,
+    alt: "Icone voiture électrique",
+    text: `Accédez à toutes les informations des bornes proches de vous.`,
+  },
+  {
+    id: 2,
+    src: Accueil2,
+    alt: "Icone borne électrique",
+    text: `Réservez une borne depuis votre téléphone ou votre ordinateur en toute simplicité. `,
+  },
+  {
+    id: 3,
+    src: Accueil3,
+    alt: "Icone voiture électrique et tablette",
+    text: `Rejoignez la communauté dynamique des utilisateurs de véhicules électriques.`,
+  },
+  {
+    id: 4,
+    src: Accueil4,
+    alt: "Icone tablette, points de recharge",
+    text: `Retrouvez toutes vos réservations en quelques clics. `,
+  },
+];
+
 function Accueil() {
+  const [currentItem, setCurrentItem] = useState(0);
+
+  useEffect(() => {
+    setCurrentItem(0);
+  }, []);
+
+  const handleDotClick = (index) => {
+    setCurrentItem(index);
+  };
+
+  const prevImage = () => {
+    setCurrentItem(
+      (prevIndex) => (prevIndex - 1 + items.length) % items.length
+    );
+  };
+
+  const nextImage = () => {
+    setCurrentItem((prevIndex) => (prevIndex + 1) % items.length);
+  };
+
+  const numberOfDots = items.length;
+
   return (
-    <section>
-      <h1> Bonjour je suis l'accueil </h1>
-      <Link to="/connexion"> Créer votre compte </Link>
-      <Link to="/inscription">Inscrivez-vous</Link>
-      <Link to="/carte"> Accéder directement à la carte </Link>
+    <section className="accueil-section">
+      <h1>
+        Restez <span className="blue"> branchés </span> à GéoCode
+      </h1>
+      <div className="accueil-container">
+        {items.length > 0 && (
+          <div className="accueil-image">
+            <img src={items[currentItem]?.src} alt={items[currentItem]?.alt} />
+          </div>
+        )}
+
+        <div className="accueil-buttons">
+          <button
+            className="navigation-buttons"
+            type="button"
+            onClick={prevImage}
+          >
+            Précédent
+          </button>
+          <div className="accueil-dots">
+            {Array.from({ length: numberOfDots }).map((_, index) => (
+              <button
+                aria-label="dots"
+                key={items.id}
+                className={`dot ${currentItem === index ? "active" : ""}`}
+                onClick={() => handleDotClick(index)}
+                tabIndex={0}
+                type="button"
+              />
+            ))}
+          </div>
+          <button
+            className="navigation-buttons"
+            type="button"
+            onClick={nextImage}
+          >
+            Suivant
+          </button>
+        </div>
+        {items.length > 0 && (
+          <div className="accueil-bottom-text">
+            <p>{items[currentItem]?.text}</p>
+          </div>
+        )}
+        <div className="accueil-bottom-buttons">
+          <Link to="/connexion" className="accueil-connexion">
+            Créer votre compte
+          </Link>
+          <Link to="/inscription" className="accueil-inscription">
+            Inscrivez-vous
+          </Link>
+          <Link to="/carte" className="accueil-link">
+            Accéder directement à la carte
+          </Link>
+        </div>
+      </div>
     </section>
   );
 }
