9596ac1b4df6c930d1e198052f68b0e247d6de00
Vincent Pasquiou <vincent.pasquiou@gmail.com>
2024-06-19 10:40:34 +0200

    add dynamic hero to all pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Menu from "./components/Menu";
-
-import hero from "./assets/images/hero-relative.png";
+import heroData from "./services/heroData";
 
 function App() {
+  const location = useLocation();
+  const page = location.pathname.replaceAll("/", "");
+  /* eslint no-plusplus: ["error", { "allowForLoopAfterthoughts": true }] */
+  function isCurrentPage(Data, path) {
+    for (let i = 0; i < Data.length; i++) {
+      if (Data[i].img.includes(path)) {
+        return Data[i].img;
+      }
+    }
+    return false;
+  }
+
   return (
     <main className="container">
       <div className="menu">
@@ -12,7 +23,7 @@ function App() {
       <div
         className="hero"
         style={{
-          backgroundImage: `url(${hero})`,
+          backgroundImage: `url(${isCurrentPage(heroData, page)})`,
         }}
       />
       <div className="content">
---
+++ client/src/services/heroData.js
@@ -0,0 +1,18 @@
+import administrateur from "../assets/images/heros/hero-administrateur.png";
+import connexion from "../assets/images/heros/hero-connexion.png";
+
+
+const heroData = [
+  {
+    id: 1,
+    name: "administrateur",
+    img: administrateur,
+  },
+  {
+    id: 2,
+    name: "connexion",
+    img: connexion,
+  },
+];
+
+export default heroData;
