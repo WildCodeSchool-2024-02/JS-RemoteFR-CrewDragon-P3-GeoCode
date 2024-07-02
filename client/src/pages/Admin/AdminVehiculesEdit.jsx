26829c0f03f07c3e111991b93c3be7235bfdb583
Vincent Pasquiou <vincent.pasquiou@gmail.com>
2024-07-02 14:03:25 +0200

    models available need to submit

--- client/src/pages/Admin/AdminVehiculesEdit.jsx
+++ client/src/pages/Admin/AdminVehiculesEdit.jsx
@@ -7,34 +7,42 @@ function AdminVehiculesEdit() {
 
   console.info(brandData);
 
-  const {
-    formState: { errors },
-    trigger,
-    watch,
-  } = useForm();
+  const { trigger, watch, register } = useForm();
 
   const watchBrand = watch("brand");
   const [selectedBrand, setSelectedBrand] = useState(null);
-
-  console.info(selectedBrand);
 
   useEffect(() => {
     const brand = brandData.find((b) => b.id === parseInt(watchBrand, 10));
     setSelectedBrand(brand);
   }, [watchBrand]);
 
+  console.info("je suis watchbrand", watchBrand);
+
   return (
     <>
-      <h1> {vehicule.name} </h1>
+      <h1> Modifier le {vehicule.name} </h1>
       <Form method="put">
+        {/* eslint-disable react/jsx-props-no-spreading */}
         <div className="form-group">
           <label htmlFor="name">Nom de la voiture</label>
-          <input type="text" id="name" onBlur={() => trigger("name")} />
+          <input
+            id="name"
+            value={vehicule.name}
+            // Validation au moment de la perte du focus
+            onBlur={() => trigger("name")}
+          />
         </div>
+
         <div className="form-group-50-50">
           <div className="form-group">
             <label htmlFor="brand">Marque de la voiture</label>
-            <select id="brand" name="brand" onBlur={() => trigger("brand")}>
+            <select
+              id="brand"
+              name="brand"
+              {...register("brand")}
+              onBlur={() => trigger("brand")}
+            >
               <option value="">Choisissez une marque</option>
               {brandData.map((brand) => (
                 <option key={brand.id} value={brand.id}>
@@ -42,14 +50,17 @@ function AdminVehiculesEdit() {
                 </option>
               ))}
             </select>
-
-            {errors.name && <p role="alert">{errors.name.message}</p>}
           </div>
 
           {selectedBrand && (
             <div className="form-group">
               <label htmlFor="model">Modèle de la voiture</label>
-              <select id="model" name="model">
+              <select
+                id="model"
+                name="model"
+                {...register("model")}
+                onBlur={() => trigger("model")}
+              >
                 <option value="">Choisissez un modèle</option>
                 {selectedBrand.models.map((model) => (
                   <option key={model.id} value={model.id}>
@@ -57,13 +68,11 @@ function AdminVehiculesEdit() {
                   </option>
                 ))}
               </select>
-
-              {errors.name && <p role="alert">{errors.name.message}</p>}
             </div>
           )}
         </div>
 
-        <button type="submit">Modifier</button>
+        <button type="submit">Send</button>
       </Form>
 
       <Form method="delete">
