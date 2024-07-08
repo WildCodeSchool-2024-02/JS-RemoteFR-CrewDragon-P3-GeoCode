18500f4ca2e6086c651f254476f45956aa7a45fd
FANNY PRETRE <fanny.pretre@gmail.com>
2024-07-08 16:09:49 +0200

    Carte & Actualités : ok

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -2,8 +2,6 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
-import "./styles/Menu.scss";
-
 function Menu() {
   const { auth } = useAuth();
   const [open, setOpen] = useState(false);
--- client/src/pages/Carte.jsx
+++ client/src/pages/Carte.jsx
@@ -4,6 +4,8 @@ import { useLoaderData } from "react-router-dom";
 import { Icon } from "leaflet";
 // import { useEffect, useState } from "react";
 
+import NavVisiteur from "../components/NavVisiteur";
+
 function Carte() {
   const terminalData = useLoaderData();
   const icon = new Icon({
@@ -13,40 +15,43 @@ function Carte() {
   });
 
   return (
-    <MapContainer
-      center={[48.866667, 2.333333]}
-      zoom={5}
-      style={{ height: "100%" }}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {terminalData.map((borne) => (
-        <Marker
-          key={borne.id}
-          position={[borne.ylatitude, borne.xlongitude]}
-          icon={icon}
-        >
-          <Popup className="popupTerminal">
-            <h1>Borne :</h1>
-            <p>{borne.name}</p>
-            <p>{borne.address}</p>
-            {borne.isBooked === 1 ? (
-              <p>Borne déjà réservé</p>
-            ) : (
-              <p>Borne Disponible</p>
-            )}
-            <h3>Prise :</h3>
-            <p>{borne.plug_type}</p>
-            <h3>chain_name :</h3>
-            <p>{borne.chain_name}</p>
-            <h3>accessibility :</h3>
-            <p>{borne.accessibility}</p>
-          </Popup>
-        </Marker>
-      ))}
-    </MapContainer>
+    <>
+      <NavVisiteur />
+      <MapContainer
+        center={[48.866667, 2.333333]}
+        zoom={5}
+        style={{ height: "100%" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {terminalData.map((borne) => (
+          <Marker
+            key={borne.id}
+            position={[borne.ylatitude, borne.xlongitude]}
+            icon={icon}
+          >
+            <Popup className="popupTerminal">
+              <h1>Borne :</h1>
+              <p>{borne.name}</p>
+              <p>{borne.address}</p>
+              {borne.isBooked === 1 ? (
+                <p>Borne déjà réservé</p>
+              ) : (
+                <p>Borne Disponible</p>
+              )}
+              <h3>Prise :</h3>
+              <p>{borne.plug_type}</p>
+              <h3>chain_name :</h3>
+              <p>{borne.chain_name}</p>
+              <h3>accessibility :</h3>
+              <p>{borne.accessibility}</p>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </>
   );
 }
 
